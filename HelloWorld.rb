885702029b798fc0a1ee316210cdee976a41f84f
sam <samanth.malladi3636@gmail.com>
2016-10-03 22:30:53 -0700

    modified ruby file from sam repo

--- HelloWorld.rb
+++ HelloWorld.rb
@@ -1,5 +1,5 @@
-# Author : samanth malladhi
+Author : samanth malladhi
 #Date : 03/sep/2016
 
 puts "Welcome to Ruby programming"
-puts "This change is a part of sam modification
+puts "This change is a part of sam modification"
